11aa8847549fcdb914ba102d57a657d51d65e188
Rodrigo Fontes <rodrifontes@gmail.com>
2022-11-17 20:09:19 -0300

    configurando variaveis de ambiente

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,12 @@
+require('dotenv/config');
 const { Client } = require('pg');
 
 const client = new Client({
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: 'postgres',
-  database: 'bolao',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 client.connect();
